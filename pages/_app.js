82c7c86be624f552236ec6ade8cd2fe6cd35928e
Wesley M. Oliveira <oliveirawesleyrj@gmail.com>
2022-06-14 15:57:51 -0300

    add default pages layout and setup fontawesome

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,18 @@
+import { ThemeProvider } from 'next-themes';
+import Script from 'next/script';
+
+import { Footer, Navbar } from '../components';
 import '../styles/globals.css';
 
-const MyApp = ({ Component, pageProps }) => <Component {...pageProps} />;
+const MyApp = ({ Component, pageProps }) => (
+  <ThemeProvider attribute="class">
+    <Navbar />
+    <div className="dark:bg-nft-dark bg-white min-h-screen">
+      <Component {...pageProps} />
+      <Footer />
+    </div>
+    <Script src="https://kit.fontawesome.com/a31ca613f9.js" crossOrigin="anonymous" />
+  </ThemeProvider>
+);
 
 export default MyApp;
