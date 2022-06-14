51526912b2d389622a5258740da2517dbbf08a14
Wesley M. Oliveira <oliveirawesleyrj@gmail.com>
2022-06-14 15:18:25 -0300

    setup eslint

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
-export default function Home() {
-  return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-    </div>
-  );
-}
+const Home = () => (
+  <div>
+    <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  </div>
+);
+
+export default Home;
