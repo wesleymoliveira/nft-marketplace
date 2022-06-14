ff08678d2aab2299fe7f0772d669291d5faee687
Wesley M. Oliveira <oliveirawesleyrj@gmail.com>
2022-06-14 19:25:54 -0300

    customize navbar

---
+++ components/Button.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Button = ({ classStyles, btnName, handleClick }) => (
+  <button onClick={handleClick} type="button" className={`nft-gradient text-sm minlg:text-lg py-2 px-6 minlg:px-8 font-poppins font-semibold text-white ${classStyles}`}>
+    {btnName}
+  </button>
+);
+
+export default Button;
