854404bff40426e4f9efeb4f9dafd7e8e0ec97f6
Wesley M. Oliveira <oliveirawesleyrj@gmail.com>
2022-07-04 18:22:21 -0300

    create contract funcs

--- contracts/NFTMarketplace.sol
+++ contracts/NFTMarketplace.sol
@@ -51,4 +51,80 @@ contract NFTMarketplace is ERC721URIStorage {
     function getListingPrice() public view returns (uint256) {
         return listingPrice;
     }
+
+    function createToken(string memory tokenURI, uint256 price)
+        public
+        payable
+        returns (uint256)
+    {
+        _tokensIds.increment();
+        uint256 newTokenId = _tokensIds.current();
+
+        _mint(msg.sender, newTokenId);
+
+        _setTokenURI(newTokenId, tokenURI);
+
+        createMarketItem(newTokenId, price);
+
+        return newTokenId;
+    }
+
+    function createMarketItem(uint256 tokenId, uint256 price) private {
+        require(price > 0, "TokenId and price must be non-zero");
+        require(msg.value == listingPrice, "Price must match listing price");
+
+        idToMarketItem[tokenId] = MarketItem(
+            tokenId,
+            payable(msg.sender),
+            payable(address(this)),
+            price,
+            false
+        );
+
+        _transfer(msg.sender, address(this), tokenId);
+
+        emit MarketItemCreated(tokenId, msg.sender, owner, price, false);
+    }
+
+    function reSellToken(uint256 tokenId, uint256 price) public payable {
+        require(
+            msg.sender == idToMarketItem[tokenId].owner,
+            "Only the owner can re-sell a token"
+        );
+        require(msg.value == listingPrice, "Price must match listing price");
+
+        idToMarketItem[tokenId].sold = false;
+        idToMarketItem[tokenId].price = price;
+        idToMarketItem[tokenId].seller = payable(msg.sender);
+        idToMarketItem[tokenId].owner = payable(address(this));
+
+        _itemsSold.decrement();
+
+        _transfer(msg.sender, address(this), tokenId);
+    }
+
+    function createMarketSales(uint256 tokenId) public payable {
+        require(
+            msg.sender == idToMarketItem[tokenId].owner,
+            "Only the owner can create a market sale"
+        );
+
+        uint256 price = idToMarketItem[tokenId].price;
+
+        require(
+            msg.value == price,
+            "Please submit the correct amount of ether to create a market sale"
+        );
+
+        idToMarketItem[tokenId].owner = payable(msg.sender);
+        idToMarketItem[tokenId].seller = payable(address(0));
+        idToMarketItem[tokenId].sold = true;
+
+        _itemsSold.increment();
+
+        _transfer(address(this), msg.sender, tokenId);
+
+        payable(owner).transfer(listingPrice);
+        payable(idToMarketItem[tokenId].seller).transfer(msg.value);
+    }
 }
